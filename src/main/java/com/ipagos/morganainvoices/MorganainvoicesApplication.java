61e7e27018aebdd449b08f648a4dd17cf847a781
eshan <eshan@ipagos.lat>
2025-10-25 13:10:28 -0600

    Fix ComponentScan issue to resolve 404 error

--- src/main/java/com/ipagos/morganainvoices/MorganainvoicesApplication.java
+++ src/main/java/com/ipagos/morganainvoices/MorganainvoicesApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Import the classes to exclude
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration; 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan; 
 
 // Add the @EnableAutoConfiguration annotation here
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) 
+@ComponentScan(basePackages = "com.ipagos.morganainvoices")
 public class MorganainvoicesApplication {
 
     public static void main(String[] args) {
